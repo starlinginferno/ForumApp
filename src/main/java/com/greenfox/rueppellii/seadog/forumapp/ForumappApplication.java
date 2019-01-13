7d0b8dfe90dd5b9c9c4fbb3881fa4632d77cf008
Lili <liliszendrei@gmail.com>
2019-01-13 21:09:23 +0100

    create basic spring project

---
+++ src/main/java/com/greenfox/rueppellii/seadog/forumapp/ForumappApplication.java
@@ -0,0 +1,14 @@
+package com.greenfox.rueppellii.seadog.forumapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForumappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ForumappApplication.class, args);
+    }
+
+}
+
